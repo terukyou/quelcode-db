9aaedd6c6962b0bc7feb5904c02cc9a1d1e4950b
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-09-04 13:20:52 +0900

    問題番号001-SQL文、画面キャプチャ

---
+++ db/db-challenge3/db-001/db-001.sql
@@ -0,0 +1,14 @@
+INSERT INTO users
+(email,password,name,self_introduction,work_phone,mobile_phone,is_deleted)
+VALUES
+('admin@labot.inc','admin','admin','このスペースの管理者です。','00-0000-0000',NULL,NULL),
+('mysql@labot.inc','mysql55','mysql',NULL,NULL,'080-1234-1253',NULL),
+('sqlserver@labot.inc','apple','sqlserver','オラクルデータベースが好きです。',NULL,NULL,NULL),
+('mariadb@labot.inc','maria','mariadb',NULL,'03-6666-5879',NULL,NULL),
+('laravel@labot.inc','lara','laravel',NULL,NULL,NULL,NULL),
+('cakephp@labot.inc','cake89','cake',NULL,'03-987-1234','090-5555-7854',NULL),
+('react@labot.inc','node','react',NULL,NULL,NULL,NULL),
+('html@labot.inc','css3','html5',NULL,NULL,'080-5849-5555',NULL),
+('php@labot.inc','php7','php','伝説のphp6',NULL,NULL,NULL),
+('javascript@labot.inc','react','js','Hello world','0120-555-458',NULL,NULL),
+('microsoft@gmail.com','google','amazon','facebook',NULL,'080-9999-5555',1)
