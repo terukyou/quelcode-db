8cb876422860dd4dff1c26cef9af8f7c4b21aaf1
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-09-07 18:26:35 +0900

    問題番号009-SQL文、画面キャプチャ

---
+++ db/db-challenge3/db-009/db-009.sql
@@ -0,0 +1,9 @@
+SELECT COUNT(chats.chatroom_id) AS '投稿数',
+    chatrooms.name AS 'チャットルーム名'
+FROM chats,
+    chatrooms
+WHERE chats.chatroom_id = chatrooms.id
+    AND chats.is_deleted <> 1
+    AND chatrooms.is_deleted<>1
+GROUP BY chats.chatroom_id
+ORDER BY COUNT(*) DESC;
