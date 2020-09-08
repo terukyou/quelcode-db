3407058ca2d2050ab7520664f931f777ca4c326f
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-09-08 22:20:10 +0900

    問題番号010の修正とそれに伴う画像キャプチャの変更

--- db/db-challenge3/db-010/db-010.sql
+++ db/db-challenge3/db-010/db-010.sql
@@ -1,18 +1,22 @@
 SELECT users.name AS 'ユーザー名',
-    chats.modified_at,
+    chats_latest AS '投稿日',
     chatrooms.name AS 'チャットルーム名'
-FROM chats,
+FROM users,
     chatrooms,
-    users
-WHERE users.id = chats.updated_by
-    AND chatrooms.id = chats.chatroom_id
+    (
+        SELECT A.chatroom_id as room_id,
+            A.updated_by as user_id,
+            chats_latest
+        FROM chats as A
+            JOIN (
+                SELECT MAX(chats.modified_at) AS chats_latest,
+                    chatroom_id
+                FROM chats
+                WHERE chats.is_deleted <> 1
+                GROUP BY chats.chatroom_id
+            ) AS max ON chats_latest = A.modified_at
+    ) AS m
+WHERE room_id = chatrooms.id
+    AND user_id = users.id
     AND users.is_deleted <> 1
     AND chatrooms.is_deleted <> 1
-    AND chats.modified_at IN (
-        SELECT MAX(chats.modified_at) AS '投稿日時'
-        FROM chats
-        WHERE chats.is_deleted <> 1
-        GROUP BY chats.chatroom_id
-    )
-ORDER BY chatrooms.id;
-
