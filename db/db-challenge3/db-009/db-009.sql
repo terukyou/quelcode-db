8cb196c99f8ebb98788b0903089599862a2b397e
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-09-11 10:48:32 +0900

    問題番号009-削除されたユーザーを含まない条件に変更＋GROUPBYをチャットルームのidに変更

--- db/db-challenge3/db-009/db-009.sql
+++ db/db-challenge3/db-009/db-009.sql
@@ -1,9 +1,11 @@
 SELECT COUNT(chats.chatroom_id) AS '投稿数',
     chatrooms.name AS 'チャットルーム名'
 FROM chats,
-    chatrooms
-WHERE chats.chatroom_id = chatrooms.id
+    chatrooms,
+    users
+    WHERE chats.chatroom_id = chatrooms.id
+    AND chats.updated_by = users.id
     AND chats.is_deleted <> 1
-    AND chatrooms.is_deleted<>1
-GROUP BY chatrooms.name
+    AND users.is_deleted <> 1
+GROUP BY chatrooms.id
 ORDER BY COUNT(*) DESC;
