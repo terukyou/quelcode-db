53c12070c39c9d9bb6d4e64e951726f9aaf29226
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-09-14 18:29:18 +0900

    WHERE内のNULLと空の抽出方法の変更

--- db/db-challenge3/db-013/db-013.sql
+++ db/db-challenge3/db-013/db-013.sql
@@ -2,12 +2,12 @@ BEGIN;
 UPDATE users
 SET is_deleted = 1
 WHERE (
-        work_phone IS NULL
-        AND mobile_phone IS NULL
-    )
-    OR(
         work_phone = ''
-        AND mobile_phone = ''
+        OR work_phone IS NULL
+    )
+    AND(
+        mobile_phone = ''
+        OR mobile_phone IS NULL
     )
     AND is_deleted <> 1;
 COMMIT;
