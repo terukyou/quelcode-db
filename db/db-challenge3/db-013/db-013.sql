0f8aa5eb4d45f6fcbc46eb1c5213a47ed0c3ca47
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-09-14 13:31:28 +0900

    問題番号013-電話の空文字抽出を追加

--- db/db-challenge3/db-013/db-013.sql
+++ db/db-challenge3/db-013/db-013.sql
@@ -1,7 +1,13 @@
 BEGIN;
 UPDATE users
 SET is_deleted = 1
-WHERE work_phone IS NULL
-    AND mobile_phone IS NULL
+WHERE (
+        work_phone IS NULL
+        AND mobile_phone IS NULL
+    )
+    OR(
+        work_phone = ''
+        AND mobile_phone = ''
+    )
     AND is_deleted <> 1;
 COMMIT;
